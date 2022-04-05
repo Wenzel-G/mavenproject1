6ee5f0ad3877d5052da3ef7a0eea3d4ae80d4956
Wenzel-G <103013271+Wenzel-G@users.noreply.github.com>
2022-04-05 09:56:09 -0400

    testing commit

---
+++ src/main/java/com/quintrix/mavenproject1/mavenproject1/Application.java
@@ -0,0 +1,9 @@
+package com.quintrix.mavenproject1.mavenproject1;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
