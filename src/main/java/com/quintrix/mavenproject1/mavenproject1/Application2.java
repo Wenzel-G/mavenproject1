8c97f0784599d55ff7ce314f1d6ba37f3aa16f02
Wenzel-G <103013271+Wenzel-G@users.noreply.github.com>
2022-04-12 12:19:06 -0400

    Added Stream assignment and starting file for JDBC assignment

--- src/main/java/com/quintrix/mavenproject1/mavenproject1/Application2.java
+++ src/main/java/com/quintrix/mavenproject1/mavenproject1/Application2.java
@@ -1,9 +1,26 @@
 package com.quintrix.mavenproject1.mavenproject1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Application2 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+    	
+    	
+        
+    	BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+    	
+    	String theText;
+    	while((theText = br.readLine())!=null) {
+    		System.out.println(theText);
+    	}
+    	
+    	System.out.println(Paths.get("").toAbsolutePath());
+    	
+    	br.close();
     }
 
 }
